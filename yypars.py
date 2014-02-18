8628d328746497a9b2a6628972c660cbfa2ba6c7
astroChasqui <ircarrascal@gmail.com>
2014-02-18 17:37:39 -0600

    radius and more options added

--- yypars.py
+++ yypars.py
@@ -11,10 +11,23 @@ import os
 logger = logging.getLogger(__name__)
 
 class SolvePars:
-    def __init__(self, db='yy02.sql3', nsigma=5, window_len=13):
+    def __init__(self, db='yy02.sql3', nsigma=5,
+                 window_len_age=13):
         self.get_isochrone_points_db = db
         self.get_isochrone_points_nsigma = nsigma
-        self.smooth_window_len = window_len
+        self.smooth_window_len_age = window_len_age
+        self.smooth_window_len_mass = 0
+        self.smooth_window_len_logl = 0
+        self.smooth_window_len_mv = 0
+        self.smooth_window_len_r = 0
+
+class PlotPars:
+    def __init__(self):
+        self.age_xlim = [0, 14]
+        self.mass_xlim = [0.7, 1.2]
+        self.logl_xlim = [-0.5, 0.5]
+        self.mv_xlim = [4.2, 5.5]
+        self.r_xlim = [0.5, 1.8]
 
 def pdf(pdf_x, ips, prob, par, smooth_window_len):
     '''Calculates a probability distribution function (PDF) for parameter par
@@ -30,6 +43,9 @@ def pdf(pdf_x, ips, prob, par, smooth_window_len):
                                              ips[par] <  x+dx)]))
     pdf_y = np.array(pdf_y)
     pdf_y = pdf_y/simps(pdf_y, pdf_x)
+    #print(len(pdf_y))
+    #if len(pdf_y) <= smooth_window_len*4:
+    #    smooth_window_len = 0
     try:
         pdf_y_smooth = smooth(pdf_y, smooth_window_len)
         pdf_y_smooth = pdf_y_smooth/simps(pdf_y_smooth, pdf_x)
@@ -63,17 +79,17 @@ def pdf(pdf_x, ips, prob, par, smooth_window_len):
     areas_right = np.array(areas_right)
 
     stats['lower_limit_1sigma'] = \
-      round(griddata(areas_left, pdf_x_left, 0.158),1)
+      griddata(areas_left, pdf_x_left, 0.158)
     stats['lower_limit_2sigma'] = \
-      round(griddata(areas_left, pdf_x_left, 0.022),1)
+      griddata(areas_left, pdf_x_left, 0.022)
     stats['upper_limit_1sigma'] = \
-      round(griddata(areas_right, pdf_x_right, 0.341),1)
+      griddata(areas_right, pdf_x_right, 0.341)
     stats['upper_limit_2sigma'] = \
-      round(griddata(areas_right, pdf_x_right, 0.477),1)
+      griddata(areas_right, pdf_x_right, 0.477)
 
     return pdf_y, pdf_y_smooth, stats
 
-def solve_one(Star, SolvePars):
+def solve_one(Star, SolvePars, PlotPars):
     '''Calculates most likely parameters of Star using YY isochrone points
     '''
     ips = get_isochrone_points(Star,
@@ -81,24 +97,50 @@ def solve_one(Star, SolvePars):
                                SolvePars.get_isochrone_points_nsigma)
     logger.info('Using {0} Y2 isochrone points'.format(len(ips['age'])))
     Star.n_yy_points = len(ips['age'])
-    ips_t = 10**ips['logt']
-    prob = np.exp(-1*((ips_t-Star.teff)/(1.414214*Star.err_teff))**2)*\
+    ips['t'] = 10**ips['logt']
+    ips['r'] = 10**(0.5*(np.log10(ips['mass'])-ips['logg']+4.437))
+    prob = np.exp(-1*((ips['t']-Star.teff)/(1.414214*Star.err_teff))**2)*\
            np.exp(-1*((ips['logg']-Star.logg)/(1.414214*Star.err_logg))**2)*\
            np.exp(-1*((ips['feh']-Star.feh)/(1.414214*Star.err_feh))**2)
 
-    #age, mass, logl, mv
-
+    #age
     ages = 0.1+np.arange(150)*0.1
     pdf_age_x = ages[np.logical_and(ages >= min(ips['age'])-0.2,
                                    ages <= max(ips['age'])+0.2)]
     pdf_age_y, pdf_age_y_smooth, Star.yyage = \
-      pdf(pdf_age_x, ips, prob, 'age', SolvePars.smooth_window_len)
+      pdf(pdf_age_x, ips, prob, 'age', SolvePars.smooth_window_len_age)
 
-    masses = 0.4+np.arange(421)*0.01
+    #mass
+    masses = 0.4+np.arange(211)*0.01
     pdf_mass_x = masses[np.logical_and(masses >= min(ips['mass'])-0.02,
                                        masses <= max(ips['mass'])+0.02)]
     pdf_mass_y, pdf_mass_y_smooth, Star.yymass = \
-      pdf(pdf_mass_x, ips, prob, 'mass', 1e10) #1e10=No smoothing
+      pdf(pdf_mass_x, ips, prob, 'mass', SolvePars.smooth_window_len_mass)
+
+    #luminosity
+    logls = -1.0+np.arange(401)*0.01
+    pdf_logl_x = logls[np.logical_and(logls >= min(ips['logl'])-0.02,
+                                      logls <= max(ips['logl'])+0.02)]
+    pdf_logl_y, pdf_logl_y_smooth, Star.yylogl = \
+      pdf(pdf_logl_x, ips, prob, 'logl', SolvePars.smooth_window_len_logl)
+
+    #absolute magnitude
+    mvs = -3.0+np.arange(1601)*0.01
+    pdf_mv_x = mvs[np.logical_and(mvs >= min(ips['mv'])-0.02,
+                                  mvs <= max(ips['mv'])+0.02)]
+    pdf_mv_y, pdf_mv_y_smooth, Star.yymv = \
+      pdf(pdf_mv_x, ips, prob, 'mv', SolvePars.smooth_window_len_mv)
+
+    #radius
+    rs = 0.4+np.arange(211)*0.01
+    pdf_r_x = rs[np.logical_and(rs >= min(ips['r'])-0.02,
+                                rs <= max(ips['r'])+0.02)]
+    pdf_r_y, pdf_r_y_smooth, Star.yyr = \
+      pdf(pdf_r_x, ips, prob, 'r', SolvePars.smooth_window_len_r)
+    #r = 10**(0.5*(np.log10(Star.yymass['most_probable'])-Star.logg+4.437))
+    #err_r = r*np.sqrt((2.0*Star.err_teff/Star.teff)**2+\
+    #                  (0.5*Star.yylogl['std'])**2)
+    #print(r, err_r)
 
     plt.figure(figsize=(7, 4))
     plt.rc("axes", labelsize=15, titlesize=12)
@@ -127,110 +169,56 @@ def solve_one(Star, SolvePars):
     #plt.savefig(fig_name, bbox_inches='tight')
     plt.close()
 
-    plt.figure(figsize=(7, 4))
+    plt.figure(figsize=(6, 12))
     plt.rc("axes", labelsize=15, titlesize=12)
     plt.rc("xtick", labelsize=14)
     plt.rc("ytick", labelsize=14)
     plt.rc("lines", markersize=10, markeredgewidth=2)
     plt.rc("lines", linewidth=2)
-    #plt.xlim([0,2])
-    plt.xlabel('Mass ($M_\odot$)')
-    plt.ylabel('Probability density')
-    plt.plot(pdf_mass_x, pdf_mass_y, color='0.9')
-    #k2 = np.logical_and(masses >= mass['lower_limit_2sigma'],
-    #                    masses <= mass['upper_limit_2sigma'])
-    #k1 = np.logical_and(masses >= mass['lower_limit_1sigma'],
-    #                    masses <= mass['upper_limit_1sigma'])
-    #plt.fill_between(masses[k2], 0 , probs[k2], color='0.8', hatch="/")
-    #plt.fill_between(masses[k1], 0 , probs[k1], color='0.6', hatch="X")
-    plt.plot([Star.yymass['most_probable'], Star.yymass['most_probable']],
-             [0, max(pdf_mass_y_smooth)], 'g--')
-    plt.plot(pdf_mass_x, pdf_mass_y_smooth, 'g')
-    plt.text(plt.xlim()[1], 0.86*plt.ylim()[1], Star.name,
-             horizontalalignment='right', size=16)
-    fig_name = Star.name.replace(' ', '_')+'_yymass'
+    plt.subplots_adjust(hspace=0.4)
+
+    for panel in np.arange(5)+1:
+        ax = plt.subplot(5,1,panel)
+        ax.get_yaxis().set_visible(False)
+        if panel == 1:
+            pdf_x, pdf_y, pdf_y_smooth = \
+              pdf_age_x, pdf_age_y, pdf_age_y_smooth
+            par = Star.yyage
+            ax.set_xlabel('Age (Gyr)')
+            ax.set_xlim(PlotPars.age_xlim)
+        if panel == 2:
+            pdf_x, pdf_y, pdf_y_smooth = \
+              pdf_mass_x, pdf_mass_y, pdf_mass_y_smooth
+            par = Star.yymass
+            ax.set_xlabel('Mass ($M_\odot$)')
+            ax.set_xlim(PlotPars.mass_xlim)
+        if panel == 3:
+            pdf_x, pdf_y, pdf_y_smooth = \
+              pdf_logl_x, pdf_logl_y, pdf_logl_y_smooth
+            par = Star.yylogl
+            ax.set_xlabel('$\log\,(L/L_\odot)$')
+            ax.set_xlim(PlotPars.logl_xlim)
+        if panel == 4:
+            pdf_x, pdf_y, pdf_y_smooth = \
+              pdf_mv_x, pdf_mv_y, pdf_mv_y_smooth
+            par = Star.yymv
+            ax.set_xlabel('$M_V$')
+            ax.set_xlim(PlotPars.mv_xlim)
+        if panel == 5:
+            pdf_x, pdf_y, pdf_y_smooth = \
+              pdf_r_x, pdf_r_y, pdf_r_y_smooth
+            par = Star.yyr
+            ax.set_xlabel('Radius ($R_\odot$)')
+            ax.set_xlim(PlotPars.r_xlim)
+        #ax.set_ylabel('Probability density')
+        ax.plot(pdf_x, pdf_y, color='0.9')
+        ax.plot([par['most_probable'], par['most_probable']],
+                [0, max(pdf_y_smooth)], 'g--')
+        ax.plot(pdf_x, pdf_y_smooth, 'g')
+
+    fig_name = Star.name.replace(' ', '_')+'_yypars'
     plt.savefig(fig_name, bbox_inches='tight')
     plt.close()
-
-
-    return
-
-    ###
-
-    plt.figure(figsize=(7, 5))
-    plt.rc("axes", labelsize=15, titlesize=12)
-    plt.rc("xtick", labelsize=14)
-    plt.rc("ytick", labelsize=14)
-    plt.rc("lines", markersize=10, markeredgewidth=2)
-    plt.rc("lines", linewidth=3)
-    plt.plot(ips_t, ips['logg'], 'y,')
-    plt.xlim(Star.teff+6*Star.err_teff, Star.teff-6*Star.err_teff)
-    plt.ylim(Star.logg+6*Star.err_logg, Star.logg-6*Star.err_logg)
-    plt.xlabel('$T_\mathrm{eff}$ (K)')
-    plt.ylabel('log g [cgs]')
-    plt.errorbar(Star.teff, Star.logg, Star.err_logg, Star.err_teff, 'green')
-    fig_name = Star.name.replace(' ', '_')+'_yyhr'
-    plt.savefig(fig_name, bbox_inches='tight')
-    plt.close()
-
-
-
-    logls = -2.0+np.arange(501)*0.01 # logl between -2 and 3 in 0.01 dex steps
-    logls = logls[np.logical_and(logls >= min(ips['logl'])-0.05,
-                                 logls <= max(ips['logl'])+0.05)]
-
-    plt.figure(figsize=(7, 4))
-    plt.rc("axes", labelsize=15, titlesize=12)
-    plt.rc("xtick", labelsize=14)
-    plt.rc("ytick", labelsize=14)
-    plt.rc("lines", markersize=10, markeredgewidth=2)
-    plt.rc("lines", linewidth=2)
-    #plt.xlim([0,2])
-    plt.xlabel('$\log\,(L/L_\odot)$')
-    plt.ylabel('Probability density')
-    plt.plot([logl['most_probable'], logl['most_probable']],
-             [0,max(probs)], 'g--')
-    plt.plot(logls, probs, 'g')
-    plt.text(0.1, 0.86*plt.ylim()[1], Star.name,
-             horizontalalignment='right', size=16)
-    fig_name = Star.name.replace(' ', '_')+'_yylogl'
-    plt.savefig(fig_name, bbox_inches='tight')
-    plt.close()
-
-    #RADIUS
-    #logR = 0.5* ( np.log10(mass['most_probable']) - Star.logg + 4.437 )
-    #R = 10**logR
-    #print (R)
-
-    plt.figure(figsize=(7, 4))
-    plt.rc("axes", labelsize=15, titlesize=12)
-    plt.rc("xtick", labelsize=14)
-    plt.rc("ytick", labelsize=14)
-    plt.rc("lines", markersize=10, markeredgewidth=2)
-    plt.rc("lines", linewidth=2)
-    #plt.xlim([0,2])
-    plt.xlabel('Mass ($M_\odot$)')
-    plt.ylabel('Probability density')
-    #k2 = np.logical_and(masses >= mass['lower_limit_2sigma'],
-    #                    masses <= mass['upper_limit_2sigma'])
-    #k1 = np.logical_and(masses >= mass['lower_limit_1sigma'],
-    #                    masses <= mass['upper_limit_1sigma'])
-    #plt.fill_between(masses[k2], 0 , probs[k2], color='0.8', hatch="/")
-    #plt.fill_between(masses[k1], 0 , probs[k1], color='0.6', hatch="X")
-    plt.plot([mass['most_probable'], mass['most_probable']],
-             [0,max(probs)], 'g--')
-    plt.plot(masses, probs, 'g')
-    #plt.text(14.2, 0.86*plt.ylim()[1], Star.name,
-    #         horizontalalignment='right', size=16)
-    fig_name = Star.name.replace(' ', '_')+'_yymass'
-    plt.savefig(fig_name, bbox_inches='tight')
-    plt.close()
-
-    #del(ips)
-    #return None
-
-    del(ips)
-
 
 def get_isochrone_points(Star, db, nsigma):
     '''Looks in the db database for isochrone points within nsigma from
