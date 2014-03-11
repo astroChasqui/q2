3b042963f23b185b8b511987ef83bc2bce03e7a9
astroChasqui <ircarrascal@gmail.com>
2014-03-11 12:18:49 -0500

    os fix 2

--- config.py
+++ config.py
@@ -2,9 +2,10 @@
 import os
 
 
-system_path = os.path.dirname(os.path.realpath(__file__))
+path = os.path.dirname(os.path.realpath(__file__))
+path = os.path.join(path, 'Data')
 
-COLORTEFF_PATH  = os.path.join(system_path, 'ColorTeff')
-MODATM_PATH     = os.path.join(system_path, 'ModelAtmospheres')
-ISOCHRONES_PATH = os.path.join(system_path, 'Isochrones')
-OTHER_PATH      = os.path.join(system_path, 'Other')
+COLORTEFF_PATH  = os.path.join(path, 'ColorTeff')
+MODATM_PATH     = os.path.join(path, 'ModelAtmospheres')
+ISOCHRONES_PATH = os.path.join(path, 'Isochrones')
+OTHER_PATH      = os.path.join(path, 'Other')
