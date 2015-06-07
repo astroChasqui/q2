97b82872fc7de45232a6bc131a559e13df33784a
Ivan Ramirez <ivan@astrochasqui.com>
2015-06-07 17:47:29 -0500

    better universal csv reader

--- tools.py
+++ tools.py
@@ -48,11 +48,8 @@ def read_csv(csv_file, file_type=None):
     'wavelength', 'species', 'ep', and 'gf'.
     """
 
-    os.system("cat "+csv_file+" | tr '\r' '\n' > q2tmp.csv")
-    csv_file = 'q2tmp.csv'
-    with open(csv_file) as f:
+    with open(csv_file, 'rU') as f:
         x = f.readlines()
-    os.unlink('q2tmp.csv')
 
     keys = [key.strip("\n") for key in x[0].split(",")]
     if (len(keys) != len(set(keys))):
